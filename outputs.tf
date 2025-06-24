7710e9ef995b6193fd344d03b349b61efa59ba0e
VM351 <nevzorovvlad@mail.ru>
2025-06-24 20:27:29 +0300

    full-terraform-code-8

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "registry" {
 }
 
 output "nlb_external_ip" {
-  description = "External IP of the NLB"
+  description = "External ip of the NLB"
   value       = yandex_lb_network_load_balancer.k8s_nlb.listener[0].external_address_spec[0].address
 }
