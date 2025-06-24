a11b3469041f9f438271f8d30cefb56807e71c62
VM351 <nevzorovvlad@mail.ru>
2025-06-24 23:31:25 +0300

    full-terraform-code-12

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
 output "registry" {
   value = yandex_container_registry.app.id
 }
-
-output "registry2" {
-  value = yandex_container_registry.app.id
-}
-
-output "registry3" {
-  value = yandex_container_registry.app.id
-}
