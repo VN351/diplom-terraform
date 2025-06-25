4e1f96719c465aa340de523b28365b90d27d234c
VM351 <nevzorovvlad@mail.ru>
2025-06-25 21:52:53 +0300

    terraform-9

--- kubernetes.tf
+++ kubernetes.tf
@@ -21,7 +21,6 @@ resource "yandex_compute_instance" "instances" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.subnets-dip[each.value.subnet].id
-    #security_group_ids = [yandex_vpc_security_group.k8s_sg.id]
     nat       = var.nat.yes
   }
 
