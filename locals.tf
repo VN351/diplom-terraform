9990b674e59799ea08fa78410fc062c1f1e2022b
VM351 <nevzorovvlad@mail.ru>
2025-06-27 00:59:50 +0300

    terraform-destroy-5-1

--- locals.tf
+++ locals.tf
@@ -3,13 +3,13 @@ locals {
     "storage.admin",
     "editor"
   ]
-/*
+
   subnets = [
     { name = "subnet-1", cidr = "10.10.10.0/24", zone = "ru-central1-a" },
     { name = "subnet-2", cidr = "10.10.40.0/24", zone = "ru-central1-b" },
     { name = "subnet-3", cidr = "10.10.50.0/24", zone = "ru-central1-d" }
   ]
- 
+/* 
   instances = [
     { name = "k8s-master", zone = "ru-central1-a", subnet = "subnet-1", cores = 2, memory = 4, core_fraction = 20, size = 15 },
     { name = "k8s-worker-1", zone = "ru-central1-b", subnet = "subnet-2", cores = 2, core_fraction = 20, memory = 6,size = 15 },
