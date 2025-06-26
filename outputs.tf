d175aa5a609120936d8693013e48d68b3e166457
VM351 <nevzorovvlad@mail.ru>
2025-06-26 21:38:35 +0300

    terraform-11

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,18 @@ output "registry" {
   value = yandex_container_registry.app.id
 }
 
+output "internal_ips" {
+  description = "Внутренние IP-адреса инстансов"
+  value = {
+    for name, instance in yandex_compute_instance.instances :
+    name => instance.network_interface[0].ip_address
+  }
+}
+
+output "external_ips" {
+  description = "Внешние IP-адреса инстансов"
+  value = {
+    for name, instance in yandex_compute_instance.instances :
+    name => instance.network_interface[0].nat_ip_address
+  }
+}
